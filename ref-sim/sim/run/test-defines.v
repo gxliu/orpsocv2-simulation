6b599048c2f94db5c1d54a4ebd3d0eb937330eab
Bony Chen <hkbonychen@gmail.com>
2015-01-07 21:48:22 +0000

    New instruction 'slwz, ssw' to load and store word-length data are added but with the same functionality as 'lwz, sw' at the current stage. The corresponding modified toolchain is in the working directory of 'openRISC-GNU-cypherDB'. The files modified involved: or1200_ctrl.v, or1200_defines.v. The reason of modification is to let hardware recongize the opcode 'slwz and ssw' (or1200_defines.v) and avoid exception plus implement the same functionalities as 'lwz and sw' (or1200_ctrl.v)

---
+++ ref-sim/sim/run/test-defines.v
@@ -0,0 +1,5 @@
+`define RTL_SIM
+`define SIMULATOR_ICARUS
+`define TEST_NAME_STRING "or1200-basic"
+`define VCD
+`define PROCESSOR_MONITOR_ENABLE_LOGS
---
+++ ref-sim/sw/tests/or1200/sim/or1200-simple.c
@@ -0,0 +1,30 @@
+/*
+ *
+ * Test run first, to check the main loop is reached and the exit mechanism
+ * functions OK
+ *
+ */
+ 
+
+#include "cpu-utils.h"
+
+int main()
+{
+  volatile int first = 0;
+  volatile int second = 1;
+  volatile int third, n;
+  int i;
+  
+  for (i=0;i<1000;i++);
+
+  for (i=0; i<=100; i++)
+    {
+      third = first + second;
+      first = second;
+      second = third;
+      report(third);
+    }
+
+  report(0x8000000d);
+  exit(0);
+}
